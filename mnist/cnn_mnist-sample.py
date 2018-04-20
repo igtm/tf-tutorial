6dc8eec591ead4f9bc9478fb644b4e6d8a7965e7
Iguchi Tomokatsu <iguchi.tomokatsu@designone.jp>
2018-04-20 13:08:32 +0900

    [mnist]

--- mnist/cnn_mnist-sample.py
+++ mnist/cnn_mnist-sample.py
@@ -2,79 +2,82 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-# Imports
 import numpy as np
 import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-# Our application logic will be added here
+
 def cnn_model_fn(features, labels, mode):
   """Model function for CNN."""
   # Input Layer
-  # 第一引数の Tensor<特徴> を 指定の形に reshapeする
-  # batch_size: -1(-1は features['x'] の数によって動的に計算されるということ。ここのbatch_sizeは hyperparameterで、チューニングできる要素の一つ。), image_width: 28, image_height: 28, channnels: 1(black: 色の数のこと)
-  # ex) batch_sizeが5: [5, 28, 28, 1] => 5 * 28 * 28 = 3920個の値が features['x']に含まれるということになる
+  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+  # MNIST images are 28x28 pixels, and have one color channel
   input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
 
-  # Convolutional Layer #1: Applies 32 5x5 filters (extracting 5x5-pixel subregions), with ReLU activation function
-  # CNNの畳み込みに関しては これがわかりやすい @see: https://qiita.com/icoxfog417/items/5fd55fad152231d706c2
-  # 畳み込み層: 特徴抽出する意味がある
-  # => [batch_size, 28, 28, 32]
+  # Convolutional Layer #1
+  # Computes 32 features using a 5x5 filter with ReLU activation.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 28, 28, 1]
+  # Output Tensor Shape: [batch_size, 28, 28, 32]
   conv1 = tf.layers.conv2d(
       inputs=input_layer,
-      filters=32, # filterの数 (2の累乗をセットすることが多い。32,64,128...)
-      kernel_size=[5, 5], # filterの大きさ/サイズ
-      padding="same", # 余白部分: valid(default) or same: sameはoutputがinputと同じwidth,heightになるようにpaddingを自動で挿入してくれる。普通にpadding無しでやると 28x28 5x5 => 24x24になってしまう。(inputsize-filtersize)/stride + 1   (28-5)/1 + 1 = 24
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
       activation=tf.nn.relu)
 
-  # Pooling Layer #1: Performs max pooling with a 2x2 filter and stride of 2 (which specifies that pooled regions do not overlap)
-  # プーリング層: データ圧縮の意味がある
-  # maxpooling: filter内の最大値を取っていく手法
-  # => [batch_size, 14, 14, 32]
-  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2) # [14,14] の半分になる
+  # Pooling Layer #1
+  # First max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 28, 28, 32]
+  # Output Tensor Shape: [batch_size, 14, 14, 32]
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
-  # Convolutional Layer #2 and Pooling Layer #2: Applies 64 5x5 filters, with ReLU activation function
-  # => [batch_size, 14, 14, 64]
+  # Convolutional Layer #2
+  # Computes 64 features using a 5x5 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 14, 14, 32]
+  # Output Tensor Shape: [batch_size, 14, 14, 64]
   conv2 = tf.layers.conv2d(
       inputs=pool1,
       filters=64,
       kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.relu)
-      
-  # => [batch_size, 7, 7, 64]
-  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2) # Pooling Layer #2: Again, performs max pooling with a 2x2 filter and stride of 2
 
-  # Dense Layer #1: 1,024 neurons, with dropout regularization rate of 0.4 (probability of 0.4 that any given element will be dropped during training)
-  # 1次元化(フラットにする)
-  # [batch_size, 1024]
+  # Pooling Layer #2
+  # Second max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 14, 14, 64]
+  # Output Tensor Shape: [batch_size, 7, 7, 64]
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+  # Flatten tensor into a batch of vectors
+  # Input Tensor Shape: [batch_size, 7, 7, 64]
+  # Output Tensor Shape: [batch_size, 7 * 7 * 64]
   pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-  # 分類できるようにdense層に繋げる
+
+  # Dense Layer
+  # Densely connected layer with 1024 neurons
+  # Input Tensor Shape: [batch_size, 7 * 7 * 64]
+  # Output Tensor Shape: [batch_size, 1024]
   dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-  # ドロップアウト:
-  # A Simple Way to Prevent Neural Networks from Overfitting
-  # ある更新で層の中のノードのうちのいくつかを無効にして（そもそも存在しないかのように扱って）学習を行い、
-  # 次の更新では別のノードを無効にして学習を行うことを繰り返します。
-  # これにより学習時にネットワークの自由度を強制的に小さくして汎化性能を上げ、
-  # 過学習を避けることができます。隠れ層においては、一般的に50%程度を無効すると良いと言われています。
-  # @see: http://sonickun.hatenablog.com/entry/2016/07/18/191656
+
+  # Add dropout operation; 0.6 probability that element will be kept
   dropout = tf.layers.dropout(
       inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-  # Dense Layer #2(Logits Layer): 10 neurons, one for each digit target class (0–9)
-  # 最後のレイヤーで、返り値を結果にする
-  # [batch_size, 10]
+  # Logits layer
+  # Input Tensor Shape: [batch_size, 1024]
+  # Output Tensor Shape: [batch_size, 10]
   logits = tf.layers.dense(inputs=dropout, units=10)
 
   predictions = {
       # Generate predictions (for PREDICT and EVAL mode)
-      "classes": tf.argmax(input=logits, axis=1), # 一番大きい値のindexを返す
+      "classes": tf.argmax(input=logits, axis=1),
       # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
       # `logging_hook`.
       "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
   }
-
   if mode == tf.estimator.ModeKeys.PREDICT:
     return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
@@ -95,6 +98,7 @@ def cnn_model_fn(features, labels, mode):
           labels=labels, predictions=predictions["classes"])}
   return tf.estimator.EstimatorSpec(
       mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
 
 def main(unused_argv):
   # Load training and eval data
@@ -134,6 +138,7 @@ def main(unused_argv):
       shuffle=False)
   eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
   print(eval_results)
+
 
 if __name__ == "__main__":
   tf.app.run()
